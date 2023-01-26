d71740a858b3cd376fdbc88b40afc3c85e27bd6f
Mingaleev-D92 <mingaleev,d92@mail.ru>
2023-01-26 22:32:57 +0300

    splash screen

---
+++ app/src/main/java/com/example/yodemo/ui/fragment/HomeViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.yodemo.ui.fragment
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @author : Mingaleev D
+ * @data : 26/01/2023
+ */
+
+class HomeViewModel:ViewModel() {
+}
---
+++ app/src/main/java/com/example/yodemo/ui/main/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.yodemo.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.yodemo.R
+import com.example.yodemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+   private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+      installSplashScreen()
+      setContentView(binding.root)
+
+   }
+}
